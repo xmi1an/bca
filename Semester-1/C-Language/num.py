d08e677c2c0c458db652d2f4bdc2d0103f64e705
xmi1an <jadavmilan009@gmail.com>
2022-12-30 11:37:51 +0530

    changes

--- Semester-1/C-Language/num.py
+++
@@ -1,7 +0,0 @@
-# create a files from name with 'Practical-26' to 'Practical-52'
-
-import os
-
-for i in range(26, 53):
-    with open('Practical-'+str(i)+'.c', 'w') as f:
-        f.write('/*  */')
