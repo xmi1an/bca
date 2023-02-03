0b1a850896d56633c559cffbc456fc66704c44ea
Mihir <mihirpatel9636@gmail.com>
2023-02-03 09:33:54 +0530

    added 2,3,4

--- Semester-4/cpp/Practical-2.cpp
+++ Semester-4/cpp/Practical-2.cpp
@@ -1,6 +1,14 @@
-/*  */
-
-#include <iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
-
-
+int main()
+{
+  //1st manipulators is endl
+  int a=10,b=20;
+  cout<<"a="<<a<<endl;
+  cout<<"b="<<b<<endl;
+  //2nd manipulators is setw
+  int a1=30,b1=50;
+  cout<<"a1 = "<<a1<<setw(10)<<"b1 = "<<b1;
+  return 0;
+}
--- Semester-4/cpp/Practical-4.cpp
+++ Semester-4/cpp/Practical-4.cpp
@@ -1,6 +1,53 @@
-/*  */
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-
+inline int sum(int a,int b)
+{
+ return a+b;
+}
+inline int sub(int a,int b)
+{
+ return a-b;
+}
+inline int mul(int a,int b)
+{
+ return a*b;
+}
+inline int division(int a,int b)
+{
+ return a/b;
+}
+inline int mod(int a,int b)
+{
+ return a%b;
+}
+int main()
+{   int a,b,choice;
+    cout<<"enter a 1st number: ";
+    cin>>a;
+    cout<<"enter a 2nd number: ";
+    cin>>b;
+    cout<<"1.addition"<<endl;
+    cout<<"2.substraction"<<endl;
+    cout<<"3.multipalction"<<endl;
+    cout<<"4.divison"<<endl;
+    cout<<"5.modulo"<<endl;
+    cout<<"6.exit"<<endl;
+    cout<<"enter a choice"<<endl;
+    cin>>choice;
+     switch(choice)
+    {
+    case 1:cout<<a<<"+"<<b<<"="<<sum(a,b);
+           break; 
+    case 2:cout<<a<<"-"<<b<<"="<<sub(a,b);
+           break;           
+    case 3:cout<<a<<"*"<<b<<"="<<mul(a,b);
+           break; 
+    case 4:cout<<a<<"/"<<b<<"="<<division(a,b);
+           break; 
+    case 5:cout<<a<<"%"<<b<<"="<<mod(a,b);
+           break;
+    case 6:exit(0);
+    default:cout<<"invalid choice"; 
+    }
+    return 0;
+}
