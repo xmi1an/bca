52e41feb6544591f51724ab7ab9e0a598530057b
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-04-30 19:15:54 +0530

    Update Practical-11.java

--- Semester-5/Java/Practicals/Practical-11.java
+++ Semester-5/Java/Practicals/Practical-11.java
@@ -1,2 +1,11 @@
 /* 11. Write a Java Program that will accept Command-line Arguments and display the same. */
 
+class practial11{
+  public static void main(String[] args) {
+    System.out.println("command line argument is: ");
+    for(int i=0;i<args.length;i++)
+    {
+        System.out.println(args[i]);
+    }
+  }
+}
