2b64e8c9d580f9d308ae4e874372c3d1288b4c22
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-04-30 18:40:59 +0530

    Update Practical-02.java

--- Semester-5/Java/Practicals/Practical-02.java
+++ Semester-5/Java/Practicals/Practical-02.java
@@ -1,2 +1,17 @@
 /* 2. Write a Java Program that will display factorial of the given number. */
 
+import java.util.Scanner;
+class practial2{
+ public static void main(String[] args) {
+    int n,sum=1,i;
+    System.out.println("Enter a number: ");
+    Scanner obj = new Scanner(System.in);
+        n=obj.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            sum=sum*i;
+        }
+        System.out.println("Factorial of "+n +" is "+sum);
+        obj.close();
+ }
+}
