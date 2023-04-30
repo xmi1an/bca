ac126da730d41b30cbd4a1f2ee791fb9ed8d31e9
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-04-30 18:53:39 +0530

    Update Practical-07.java

--- Semester-5/Java/Practicals/Practical-07.java
+++ Semester-5/Java/Practicals/Practical-07.java
@@ -1,2 +1,16 @@
 /* 7. Write a Java Program that will display the Sum of 1+1/2+1/3…..+1/n. */
 
+import java.util.Scanner;
+class practial7{
+    public static void main(String[] args) {
+     float i,n;double sum=0;
+     System.out.println("Enter a number: ");
+     Scanner s= new Scanner(System.in);
+     n=s.nextFloat();
+     for(i=1;i<=n;i++)
+     {
+        sum=sum+1.0/i;
+     }
+     System.out.println("sum of series is "+ sum);
+    }
+}
