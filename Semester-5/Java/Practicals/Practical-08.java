7b248065a5fd7d9410717e03f5c664631a1d7665
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-04-30 19:10:05 +0530

    Update Practical-08.java

--- Semester-5/Java/Practicals/Practical-08.java
+++ Semester-5/Java/Practicals/Practical-08.java
@@ -1,2 +1,25 @@
 /* 8. Write a Java Program that check weather the given no is prime or not. */
 
+import java.util.Scanner;
+class practial8{
+    public static void main(String[] args){
+        int n,i,count=0;
+        System.out.println("Enter a number: ");
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        for(i=1;i<=n;i++)
+        {
+         if(n%i==0)
+         {
+            count++;
+         }
+        }
+        if(count==2)
+        {
+            System.out.println(n+" is prime number");
+        }
+        else{
+            System.out.println(n+" is not prime number");
+        }
+    }
+}
