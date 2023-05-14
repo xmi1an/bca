29f04fc64751f7d724f1b6376b2444b7784bf3ea
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-05-14 12:17:22 +0530

    Update Practical-12.java

--- Semester-5/Java/Practicals/Practical-12.java
+++ Semester-5/Java/Practicals/Practical-12.java
@@ -1,2 +1,12 @@
 /* 12. Write a Java Program to sort the elements of an array in Ascending Order. */
 
+import java.util.Arrays;
+class practial12 {
+   public static void main(String[] args) {
+    int a[]={54,78,45,65,89,1,4};
+    Arrays.sort(a);
+    for(int i=0;i<=7;i++){
+    System.out.println(a[i]);
+    }
+   }
+}
