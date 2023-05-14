9dd0c00154c5929cd4b627d9ea151b34043e4db9
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-05-14 12:36:56 +0530

    Update Practical-18.java

--- Semester-5/Java/Practicals/Practical-18.java
+++ Semester-5/Java/Practicals/Practical-18.java
@@ -1,2 +1,15 @@
-/* 17. Write a java program which shows the use of Methods Overloading. */
+/* 18. Write a java program which implements the Default Constructors */
+class Check{
+    public Check(){
+        System.out.println("Hello! I Am Default Constructer...");
+    }
 
+    public void close() {
+    }
+}
+class practial18{
+    public static void main(String[] args) {
+        Check C= new Check();
+        C.close();
+    }
+}
