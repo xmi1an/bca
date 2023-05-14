d73b45fb033e25f9bcd856353f2da656226a3f92
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-05-14 12:39:32 +0530

    Update Practical-19.java

--- Semester-5/Java/Practicals/Practical-19.java
+++ Semester-5/Java/Practicals/Practical-19.java
@@ -1,2 +1,12 @@
-/* 18. Write a java program which implements the Default Constructors. */
-
+/* 19.Write a java program which implements the Parameterized Constructors. */
+class Parameter{
+    Parameter(int a,int b){
+        int c=a+b;
+        System.out.println("Sum of two number: "+c);
+    }
+}
+class practial19 {
+   public static void main(String[] args) {
+    Parameter p1=new Parameter(4,6);
+   }
+}
