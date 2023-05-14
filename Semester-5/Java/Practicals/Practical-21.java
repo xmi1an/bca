3030995c1c51963feae0016e656e4566ff7e79cb
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-05-14 12:41:53 +0530

    Update Practical-21.java

--- Semester-5/Java/Practicals/Practical-21.java
+++ Semester-5/Java/Practicals/Practical-21.java
@@ -1,2 +1,21 @@
-/* 20. Write a java program which implements the Overloading of Constructors. */
-
+/* 21.Write a java program which explains the concept of Single Inheritance. */
+class Base{
+    int a=2,b=3;
+    public void add(){
+        System.out.println("SUM OF "+a+"+"+b+"="+(a+b));
+    }
+}
+class Derive extends Base
+{
+  int p=10,q=10;
+  public void mul(){
+    System.out.println("MULTIPLATION OF "+p+"*"+q+"="+(p*q));
+  }
+}
+class practial21{
+    public static void main(String[] args) {
+        Derive d=new Derive();
+        d.mul();
+        d.add();
+    }
+}
