e0585ae9df769354b509723edd39d3df705e4d13
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-08-07 17:52:25 +0530

    Update Practical-24.java

--- Semester-5/Java/Practicals/Practical-24.java
+++ Semester-5/Java/Practicals/Practical-24.java
@@ -1,2 +1,21 @@
-/* 23. Write a java program which explains the concept of Hierarchical Inheritance. */
-
+/* 24.Write a java program which shows the Method Overriding.  */
+class Base{
+    void Display()
+    {
+        System.out.println("hello i am base class...."+"\n");
+    }
+}
+class Derive extends Base{
+    void Display()
+    {
+        System.out.println("hello i am derive class...."+"\n");
+    }
+}
+class practial24 {
+    public static void main(String[] args) {
+        Base b1=new Base();
+        b1.Display();
+        Base b2=new Derive();
+        b2.Display();
+    }
+}
