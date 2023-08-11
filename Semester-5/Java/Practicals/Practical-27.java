2a1f5ea6d9a4aa58c24cfecdd98a733024fdde11
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-08-11 21:59:26 +0530

    Update Practical-27.java

--- Semester-5/Java/Practicals/Practical-27.java
+++ Semester-5/Java/Practicals/Practical-27.java
@@ -1,2 +1,25 @@
-/* 26. Write a Java Program to implement abstract class and abstract method. */
-
+/* 27. Write a java program which implements Interface.  */
+interface Area{
+    final float PI=3.14f;
+    float findArea(int x);
+}
+class Circle implements Area{
+    float findArea(int x)
+    {
+       return (PI*x*x);
+    }
+}
+class Square implements Area{
+    float findArea(int x)
+    {
+        return x*x;
+    }
+}
+class practial27{
+    public static void main(String[] args) {
+        Circle c=new Circle();
+        System.out.println(c.findArea(10));
+        Square s=new Square();
+        System.out.println(s.findArea(10));
+    }
+}
