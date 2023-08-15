e69fdadaa6abb7f7c1f3dad47370d5d9ad433255
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-08-15 21:25:14 +0530

    Update Practical-06.java

--- Semester-5/Java/Practicals/Practical-06.java
+++ Semester-5/Java/Practicals/Practical-06.java
@@ -1,16 +1,19 @@
 /* 6. Write a Java Program to find the sum of the digits of given number. */
-
+//Write a Java Program to find the sum of the digits of given number. 
 import java.util.Scanner;
 class practial6{
     public static void main(String[] args){
-        int a,b;
+        int a,sum=0,r;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a 1st number: ");
         a=sc.nextInt();
-        System.out.println("Enter a 2nd number: ");
-        b=sc.nextInt();
-        int c=a+b;
-        System.out.println("sum="+c);
+        while(a>0)
+        {
+            r=a%10;
+            sum=sum+r;
+            a=a/10;
+        }
+        System.out.println("sum="+sum);
         sc.close();
     }
 }
