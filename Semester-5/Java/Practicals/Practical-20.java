7ac47b88cfe0984a9bcb8201374942ac2265b2e6
MIHIRPATEL1234 <122772026+MIHIRPATEL1234@users.noreply.github.com>
2023-08-21 19:18:09 +0530

    Update Practical-20.java

--- Semester-5/Java/Practicals/Practical-20.java
+++ Semester-5/Java/Practicals/Practical-20.java
@@ -1,2 +1,27 @@
-/* 19. Write a java program which implements the Parameterized Constructors. */
-
+//. Write a java program which implements the Overloading of Constructors.
+class A{
+    A(int a)
+    {
+        System.out.println("Circle");
+    }
+    A(int a,int b)
+    {
+        System.out.println("Line");
+    }
+    A(int a,int b,int c)
+    {
+        System.out.println("Triangle");
+    }
+    A(int a,int b,int c,int d)
+    {
+        System.out.println("Square");
+    }
+}
+class practial20 {
+    public static void main(String[] args) {
+        A a=new A(1);  
+        A b=new A(1,1);  
+        A c=new A(1,1,1);  
+        A d=new A(1,1,1,1);  
+    }
+}
