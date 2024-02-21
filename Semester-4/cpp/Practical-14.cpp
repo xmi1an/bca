ac06d39c9b7b9d10821c52e253b01c05f816d5d8
NAYANSINDHI <nayansindhi2004@gmail.com>
2024-02-21 20:21:06 +0530

    Completeprogram

--- Semester-4/cpp/Practical-14.cpp
+++ Semester-4/cpp/Practical-14.cpp
@@ -1,6 +1,56 @@
-/*  */
+#include <iostream> // include library for input and output
 
-#include <iostream>
-using namespace std;
+// create class Data
+class Data {
+private:
+    int data_; // create private variable data_
 
+public:
+    // define get() and set() functions to access data_ privately
+    int get() {
+        return data_;
+    }
+    void set(int data) {
+        data_ = data;
+    }
+};
 
+// create a function outside of class Data to swap two objects of type Data
+void swapData(Data &a, Data &b) {
+    // create a temporary variable and store a's data in it
+    int temp = a.get();
+
+    // set a's data to be b's
+    a.set(b.get());
+
+    // set b's data to be a's (i.e. the temporary value)
+    b.set(temp);
+}
+
+// main function
+int  main() {
+    // create two Data objects
+    Data objA;
+    Data objB;
+
+    // initialize objA's data_ with value 5
+    objA.set(5);
+
+    // initialize objB's data_ with value 10
+    objB.set(10);
+
+    // print original data_ values
+    std::cout << "Original values:\n";
+    std::cout << "objA's data: " << objA.get() << "\n";
+    std::cout << "objB's data: " << objB.get() << "\n";
+
+    // swap objA and objB's data_
+    swapData(objA, objB);
+
+    // print out new data_ values
+    std::cout << "New values:\n";
+    std::cout << "objA's data: " << objA.get() << "\n";
+    std::cout << "objB's data: " << objB.get() << "\n";
+
+    return 0;
+}
